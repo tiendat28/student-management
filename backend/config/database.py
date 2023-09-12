cc3f8472a402a7450d9fd395504746ac8a685802
banh <bangtx@otani-trading.com>
2023-09-12 17:45:09 +0700

    test

--- backend/config/database.py
+++ backend/config/database.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
-    f"postgresql://postgres:280200@localhost:5432/studentmanager"
+    f"postgresql://postgres:postgres@db:5432/default"
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- backend/schemas/subject.py
+++ backend/schemas/subject.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
+from typing import List
 
 class SubjectBase(BaseModel):
     name: str
     class_room : str
     timetable : str
     teacher_id: int
-    student_ids: list[int]
+    student_ids: List[int]
 
 class SubjectCreate(SubjectBase):
     pass
